de84cb1d1a4fbee81a0fcbf7a2c2e20eff977012
meauh <mail.bibomoh@gmail.com>
2025-05-07 20:23:11 +0100

    style: implement modal bottom sheet for modes. - style: notime section (inkwell). - style: control section (cupertino buttons).

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(NotimeApp());
@@ -82,6 +84,84 @@ class _MainUIState extends State<MainUI> {
     resetOnly();
   }
 
+  void _setMode(int mode) {
+    modeIndex = mode;
+    setState(() {
+      startTimeMs = defaultStartTimeMs[modeIndex].$1;
+      timeLeftMs = defaultStartTimeMs[modeIndex].$1;
+    });
+    resetOnly();
+    Navigator.pop(context);
+  }
+
+  void _showSheetUI() {
+    showModalBottomSheet(
+      context: context,
+      builder:
+          (cntx) => Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Text(
+                  "Pick your mode:",
+                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 16),
+                ),
+                SizedBox(
+                  height: 400,
+                  child: ListWheelScrollView(
+                    itemExtent: 64,
+                    diameterRatio: 1.2,
+                    children: [
+                      //TODO Create mode Object with active state
+                      SizedBox.expand(
+                        child: InkWell(
+                          onTap: () => _setMode(0),
+                          child: Center(
+                            child: Text("${defaultStartTimeMs[0].$2}"),
+                          ),
+                        ),
+                      ),
+                      SizedBox.expand(
+                        child: InkWell(
+                          onTap: () => _setMode(1),
+                          child: Center(
+                            child: Text("${defaultStartTimeMs[1].$2}"),
+                          ),
+                        ),
+                      ),
+                      SizedBox.expand(
+                        child: InkWell(
+                          onTap: () => _setMode(2),
+                          child: Center(
+                            child: Text("${defaultStartTimeMs[2].$2}"),
+                          ),
+                        ),
+                      ),
+                      SizedBox.expand(
+                        child: InkWell(
+                          onTap: () => _setMode(3),
+                          child: Center(
+                            child: Text("${defaultStartTimeMs[3].$2}"),
+                          ),
+                        ),
+                      ),
+                      SizedBox.expand(
+                        child: InkWell(
+                          onTap: () => _setMode(4),
+                          child: Center(
+                            child: Text("${defaultStartTimeMs[4].$2}"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+    );
+  }
+
   @override
   void dispose() {
     timer?.cancel();
@@ -96,69 +176,88 @@ class _MainUIState extends State<MainUI> {
 
     return Scaffold(
       backgroundColor: bgcolor,
-      body: GestureDetector(
-        onTap: resetAndStart,
-        onLongPress: resetOnly,
-        behavior:
-            HitTestBehavior.opaque, // ensures tap events go through empty areas
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.transparent, // this can be transparent if you prefer
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                minutes > 0
-                    ? '${minutes.toString().padLeft(2, '0')}'
-                    : '${seconds.toString().padLeft(2, '0')}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 96,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-              Text(
-                minutes > 0
-                    ? '${seconds.toString().padLeft(2, '0')}.${(millis ~/ 10)}'
-                    : '.${millis.toString().padLeft(2, '0')}',
-                style: TextStyle(
-                  color: Colors.white70,
-                  fontSize: 56,
-                  fontWeight: FontWeight.w900,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      
-      persistentFooterButtons: [
-        Row(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        padding: EdgeInsets.all(8),
+        child: Column(
+          spacing: 8,
           children: [
-            Spacer(),
-              Text(
-                defaultStartTimeMs[modeIndex].$2,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(16),
+                  onTap: resetAndStart,
+                  onLongPress: resetOnly,
+                  splashColor: const Color.fromARGB(75, 255, 255, 255),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        minutes > 0
+                            ? '${minutes.toString().padLeft(2, '0')}'
+                            : '${seconds.toString().padLeft(2, '0')}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 96,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      Text(
+                        minutes > 0
+                            ? '${seconds.toString().padLeft(2, '0')}.${(millis ~/ 10)}'
+                            : '.${millis.toString().padLeft(2, '0')}',
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 56,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-            ),
-            Spacer(),
-            IconButton.filled(
-              onPressed: _cycleMode,
-              icon: Icon(Icons.style),
-              tooltip: 'Cycle',
-              style: IconButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.lightGreen[700],
-                iconSize: 32,
               ),
+            ),
+            Row(
+              spacing: 16,
+              children: [
+                Expanded(
+                  child: CupertinoButton(
+                    color: Colors.white70,
+                    onPressed: _showSheetUI,
+                    child: Text(
+                      defaultStartTimeMs[modeIndex].$2,
+                      style: TextStyle(color: Colors.lightGreen[800]),
+                    ),
+                  ),
+                ),
+                CupertinoButton(
+                  color: Colors.white70,
+                  onPressed: _cycleMode, //TODO fullscreen method is missing
+                  child: Icon(
+                    Icons.swap_vert_rounded,
+                    // Icons.open_in_full_rounded,
+                    // Icons.volume_up_rounded,
+                    color: Colors.lightGreen[800],
+                  ),
+                ),
+                // IconButton.filled(
+                //   onPressed: _showSheetUI,
+                //   icon: Icon(Icons.style),
+                //   tooltip: 'Cycle',
+                //   style: IconButton.styleFrom(
+                //     backgroundColor: Colors.white,
+                //     foregroundColor: Colors.lightGreen[700],
+                //     iconSize: 32,
+                //   ),
+                // ),
+              ],
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 }
